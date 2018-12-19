fba422ea898c2855f7e5fe5d80070010d3cd414e
Brooke Meleah <brooke.meleah@fluxfederation.com>
2018-12-20 11:44:58 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('NDQ2NDQyNzkxMTg3MzgyMjcz.DvxJdQ.tI5zIYufurpR308x4tjBJr9Z24A');
